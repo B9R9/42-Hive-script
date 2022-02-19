41dc25fe7ed0fb35aa2ab604c19db2a724ef6e42
B9R9 <baptiste.riffard@gmail.com>
2022-02-19 20:28:09 +0200

    Update .sauvegarde.sh  add: delete oldfolder

--- .sauvegarde.sh
+++ .sauvegarde.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 
+delete_olderfolder()
+{
+	filenumber=$(find /home/chopper/sauvegarde -maxdepth 1 -type d | grep -v .git | wc -l)
+	if [ $filenumber > 2 ]; then
+		olderfolder=$(find /home/chopper/sauvegarde -maxdepth 1 -type d -exec stat --format="%y %b %n" {} \; | grep -v .git | sort | awk '{print$5}' | sed -n '1p')
+		rm -rf $olderfolder
+	fi
+}
+
 backup_local()
 {
-	for line in $(find /home/chopper/workspace -type f -mtime -1 |  grep -v .git)
+	if [ ! -e "/home/chopper/sauvegarde" ]; then
+		git clone git@github.com:B9R9/backup /home/chopper/sauvegarde
+		touch ./sauvegarde/backup.log
+	fi
+	for line in $(find /home/chopper/workspace -type f -mmin 60 |  grep -v .git)
 	do
-		if [ ! -e "/home/chopper/sauvegarde" ]; then
-			git clone git@github.com:B9R9/backup /home/chopper/sauvegarde
-			touch ./sauvegarde/savedfilelist.txt
+		subfolder=$(date)
+		folder=$(date +%d%b%g)
+		if [ ! -e /home/chopper/sauvegarde/"$folder" ]; then
+			mkdir /home/chopper/sauvegarde/"$folder"
 		fi
-		jour=$(date)
-		if [ ! -e ./sauvegarde/"$jour" ]; then
-			mkdir ./sauvegarde/"$jour"
+		if [ ! -e /home/chopper/sauvegarde/"$folder"/"$subfolder" ]; then
+			mkdir /home/chopper/sauvegarde/"$folder"/"$subfolder"
 		fi
-		cp $line ./sauvegarde/"$jour"
-		if [ $( grep -L "$jour" ./sauvegarde/savedfilelist.txt) ]; then
-			date >> ./sauvegarde/savedfilelist.txt
-			echo -e "Path des fichiers sauvegardes:\n" >> ./sauvegarde/savedfilelist.txt
+		cp $line /home/chopper/sauvegarde/"$folder"/"$subfolder"
+		if [ $( grep -L "$subfolder" /home/chopper/sauvegarde/savedfilelist.txt) ]; then
+			date >> /home/chopper/sauvegarde/backup.log
+			echo -e "Path des fichiers sauvegardes:\n" >> /home/chopper/sauvegarde/backup.log
 		fi
-		echo -e "\t - $line" >> ./sauvegarde/savedfilelist.txt
+		echo -e "\t - $line" >> /home/chopper/sauvegarde/backup.log
 	done
-	echo -e "\n================================================\n" >>./sauvegarde/savedfilelist.txt
+	echo -e
+	"\n================================================\n">>/home/chopper/sauvegarde/backup.log
 }
 
 backup_git()
 {
 	cd /home/chopper/sauvegarde/
 	git add .
-	git commit -m "back up $jour"
+	git commit -m "BACK UP: $subfolder"
 	git push
 }
 
 option()
 {
-	echo "Usage: ./  sauvegarde.sh [option]"
+	echo "Usage: ./sauvegarde.sh [option]"
 	echo "Option available:"
 	echo -e "\tlocal:   save everyfile in the workspace directory to another directory."
 	echo -e "\tdistant: save file to github repository."
@@ -42,18 +56,16 @@ option()
 
 if [ $1 = 'local' ]; then
 	backup_local
+	delete_olderfolder
 elif [ $1 = 'distant' ]; then
 	backup_local
+	delete_olderfolder
 	cd /home/chopper/sauvegarde/
 	backup_git
 elif [ $1 = 'help' ]; then
 	option 
 elif [ $1 = 'setup' ]; then
-	echo " By default the programm lookinf for file who has been modified the
-day in a specifique directory. This can be change line 5 of the programm by
-changing the path of the directory and the option mtime. If you want looking
-for file modifef the last 5 minutes, changed -mtime by mmin following by the
-number -n(number of minutes). Every path can be can for your convenance."
+	echo " Ecrire une versionman de shell"
 else
 	option
 fi
